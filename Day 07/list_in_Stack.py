36a8c49567cdef334f2d8a4f8ba0efca8b73fc18
Sajjatul Karim <62699393+sajjatulkarim@users.noreply.github.com>
2025-10-14 16:00:18 +0600

    Add files via upload
    List advanced methods

---
+++ Day 07/list_in_Stack.py
@@ -0,0 +1,13 @@
+stack = []
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+stack.append(5)
+stack.append(6)
+stack.append(7)
+print(f"stack is {stack}") 
+
+stack.pop() #Output: [1, 2, 3]
+
+print(f"stack after pop is {stack[0:5]}")
---
+++ Day 07/list_in_queue.py
@@ -0,0 +1,14 @@
+queue = []
+
+queue.append(1)
+queue.append(2)
+queue.append(3)
+queue.append(4)
+queue.append(5)
+queue.append(6)
+
+print(f"queue is {queue}")
+
+
+queue.pop(3) #Output: [5, 6]
+print(f"queue after pop is {queue}")
