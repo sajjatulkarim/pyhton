15ca9792311f9b36c22d2a0ebdd04b749894b213
Sajjatul Karim <62699393+sajjatulkarim@users.noreply.github.com>
2025-10-18 13:12:19 +0600

    Add files via upload

---
+++ Day 9/function return type/list_fn_return.py
@@ -0,0 +1,20 @@
+def ranks():
+    student1 = [10,9,5]
+    student2 = [3,8,5]
+    return ranks
+print(ranks())
+print(type(ranks()))
+
+
+
+def ranks():
+    student1 = [10,9,5]
+    student2 = [3,8,5]
+    return student1, student2
+
+x,y = ranks()
+print(x)
+print(y)
+print(type(x))
+print(ranks())
+print(type(ranks()))
